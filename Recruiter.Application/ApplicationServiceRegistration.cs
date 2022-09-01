58d527fe284992ac98c75a60cb48816991ce1018
n.morbedadze <n.morbedadze@aldagi.ge>
2022-09-01 16:49:39 +0400

    added mediatr

--- Recruiter.Application/ApplicationServiceRegistration.cs
+++ Recruiter.Application/ApplicationServiceRegistration.cs
@@ -1,12 +1,15 @@
 ﻿using System.Reflection;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Recruiter.Application;
 
 public static class ApplicationServiceRegistration
 {
-    public static void ConfigureApplicationServices(this IServiceCollection services)
+    public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        return services;
     }
 }
