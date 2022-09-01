d7303cc0a27773ef1a38ec10773c349a7a93ca59
n.morbedadze <n.morbedadze@aldagi.ge>
2022-09-01 15:47:59 +0400

    Added IGenericRepository.cs, ICandidateRepository.cs

---
+++ Recruiter.Application/Contracts/Persistence/ICandidateRepository.cs
@@ -0,0 +1,7 @@
+﻿using Recruiter.Domain;
+
+namespace Recruiter.Application.Contracts.Persistence;
+
+public interface ICandidateRepository : IGenericRepository<Candidate>
+{
+}
---
+++ Recruiter.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Recruiter.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> Get(int id);
+    Task<IReadOnlyList<T>> GetAll();
+    Task<T> Add(T entity);
+    Task<bool> Exists(int id);
+    Task<T> Update(T entity);
+    Task<T> Delete(T entity);
+}
