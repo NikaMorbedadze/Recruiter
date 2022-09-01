73d8c4cba89ea8da3768f99b7e29058326f47731
n.morbedadze <n.morbedadze@aldagi.ge>
2022-09-01 16:43:26 +0400

    added AutoMapper, MappingProfile.cs, ApplicationServiceRegistration.cs

---
+++ Recruiter.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Recruiter.Application.DTOs.Candidate;
+using Recruiter.Domain;
+
+namespace Recruiter.Application.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Candidate, CandidateDto>().ReverseMap();
+    }
+}
